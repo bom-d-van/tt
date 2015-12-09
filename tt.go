62c14902466edb7ce04fcbf887c8da6c0ff77920
bom_d_van <bom.d.van@gmail.com>
2015-12-09 21:51:54 +0900

    Add tt.go

---
+++ tt.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"io/ioutil"
+	"log"
+	"os"
+
+	htemplate "html/template"
+	ttemplate "text/template"
+)
+
+func main() {
+	typ := flag.String("t", "text", "specify template type: t/text, h/html")
+	flag.Parse()
+	if len(flag.Args()) != 2 {
+		println("usage: tt template json")
+		os.Exit(1)
+	}
+
+	tmpl, err := ioutil.ReadFile(flag.Args()[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+	dataSrc, err := ioutil.ReadFile(flag.Args()[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	var data interface{}
+	if err := json.Unmarshal(dataSrc, &data); err != nil {
+		log.Fatal(err)
+	}
+
+	if *typ == "h" || *typ == "html" {
+		if err := htemplate.Must(htemplate.New("main").Parse(string(tmpl))).Execute(os.Stdout, data); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		if err := ttemplate.Must(ttemplate.New("main").Parse(string(tmpl))).Execute(os.Stdout, data); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
